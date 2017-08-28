448116a2e387a093a2c1c8f241f706a84c848b44
MarcoRios <marcoriosgtz98@gmail.com>
2017-08-28 10:10:48 -0500

    Practica 2

---
+++ src/Practica2/SNode.java
@@ -0,0 +1,19 @@
+package Practica2;
+
+
+public class SNode<T> {
+	public T value;
+	public SNode<T> next;
+	
+	public SNode()
+	{
+		value = null;
+		next = null;
+	}
+	
+	public SNode(T value)
+	{
+		this.value = value;
+		next = null;
+	}
+}
