3bbe874addc9d39f699daf2870c32e5b204a61f1
MarcoRios <marcoriosgtz98@gmail.com>
2017-10-02 11:25:06 -0500

    Busqueda binaria

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,9 @@
  */
 public class Main {
     
-            int[] lista={1,4,7,9,10,143};
+    
 
-    public int linealSearch(int lista[]){
+    public static int linealSearch(int lista[]){
          
         int t= 5;
             for(int x=0;x>=lista.length;x++){
@@ -24,6 +24,26 @@ public class Main {
             }
             return -1;
         }
+    
+    public static int binarySearch(int lista[]){
+        int t=10;
+        int min = 0;
+        int max = lista.length-1;
+        while(min<=max){
+            int mid = (min+max)/2;
+            if (t==lista[mid]){
+            System.out.println(mid);
+                
+                return mid;
+            }else if (t<lista[mid]){
+                max=mid-1;
+            }else if (t>lista[mid]){
+                min=mid+1;
+            }
+        }
+        return -1;
+    }
+    
     
     public static void main(String[] args){
         /*IntegerLinkedList iList = new IntegerLinkedList();
@@ -64,8 +84,8 @@ public class Main {
         System.out.println(iList.getLast());*/
         
         //Busqueda lineal
-        
-                
+        int[] lista={1,4,7,9,10,143};
+               binarySearch(lista); 
     
         
         
